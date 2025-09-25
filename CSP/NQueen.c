3f615c88f1f01cc4a02a27fb78a12e463fa889c2
sanavi-rahane <sanavirahane2005@gmail.com>
2025-09-25 10:23:47 +0530

    changes

--- CSP/NQueen.c
+++ CSP/NQueen.c
@@ -2,12 +2,11 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define N 4   // change this to solve for other n (e.g. 8)
+#define N 4   
 
-char board[N][N];   // chessboard
+char board[N][N];  
 int solution = 0;
 
-// check if it's safe to place a queen at board[row][col]
 bool isSafe(int row, int col, int n)
 {
     int i, j;
@@ -35,12 +34,12 @@ bool isSafe(int row, int col, int n)
     return true;
 }
 
-// recursive function to place queens row by row
+
 void nQueens(int row, int n)
 {
     if (row == n)
     {
-        // print one solution
+        
         solution++;
         for (int i = 0; i < n; i++)
         {
@@ -65,7 +64,7 @@ void nQueens(int row, int n)
 
 int main(void)
 {
-    // initialize board with '.'
+    
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             board[i][j] = '.';
